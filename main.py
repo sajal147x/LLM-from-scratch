623b743d8664157d91da7a05f17646a19461dd35
sajal gupta <sajal147x@gmail.com>
2026-02-10 17:01:16 -0600

    using openai open source tokenizer

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import re
 from SimpleTokenizer import SimpleTokenizerV1
+from importlib.metadata import version
+import tiktoken
+
 
 
 if __name__ == '__main__':
+    # #PRE TRAINING WITH BOOK VERDICT
+    # with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    #     raw_text = f.read()
+    #     preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+    # preprocessed = [item for item in preprocessed if item.strip()]
+    # all_tokens = sorted(set(preprocessed)) #using set to remove duplicates
+    #
+    # all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+    #
+    # vocab_size = len(all_tokens)
+    #
+    # #PRE TRAINED VOCAB
+    # vocab = {token:integer for integer, token in enumerate(all_tokens)}
+    #
+    # #TESTING PRE TRAINED VOCAB WITH NEW TEXT
+    # tokenizer = SimpleTokenizerV1(vocab)
+    text1 = "Conor wins this fight by knockout"
+    text2 = "He is the new double champ"
+    text = " <|endoftext|> ".join((text1, text2))
 
-    #PRE TRAINING WITH BOOK VERDICT
-    with open("the-verdict.txt", "r", encoding="utf-8") as f:
-        raw_text = f.read()
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-    preprocessed = [item for item in preprocessed if item.strip()]
-    all_words = sorted(set(preprocessed)) #using set to remove duplicates
-    vocab_size = len(all_words)
-    #PRE TRAINED VOCAB
-    vocab = {token:integer for integer, token in enumerate(all_words)}
+    tokenizerGPT = tiktoken.get_encoding("gpt2")
+    integers = tokenizerGPT.encode(text, allowed_special={"<|endoftext|>"})
+    print(integers)
+    strings = tokenizerGPT.decode(integers)
+    print(strings)
 
-    #TESTING PRE TRAINED VOCAB WITH NEW TEXT
-    tokenizer = SimpleTokenizerV1(vocab)
-    text = "Hello, Do you like tea?"
-    ids = tokenizer.encode(text)
-    print(ids)
-    print(tokenizer.decode(ids))
 
 
 
