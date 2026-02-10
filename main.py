dc8ef0721fb05bc81fdc697b66984aa6798c9ef1
sajal gupta <sajal147x@gmail.com>
2026-02-10 11:13:45 -0600

    simple encoding and decoding of text (tokenization)

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+from SimpleTokenizer import SimpleTokenizerV1
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    with open("the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+
+    preprocessed = [item for item in preprocessed if item.strip()]
+
+    all_words = sorted(set(preprocessed)) #using set to remove duplicates
+    vocab_size = len(all_words)
+
+    vocab = {token:integer for integer, token in enumerate(all_words)}
+
+    tokenizer = SimpleTokenizerV1(vocab)
+    text = "Hello, Do you like tea?"
+    ids = tokenizer.encode(text)
+    print(ids)
+    print(tokenizer.decode(ids))
+
+
+
+
